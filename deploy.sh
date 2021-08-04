25fb1f351ae0ffb4428467518ddf23e368cbdacc
卢哥 <luzhanghong.lzh@alibaba-inc.com>
2021-08-04 22:05:37 +0800

    update

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+rm -rf _site && rm -rf .jekyll-cache
+
+git add --all && git commit -m 'update' && git push origin master
+
+bundle install && jekyll build
+
+cp -R _site/* ../loji44.github.io
+
+cd ../loji44.github.io
+
+git add --all && git commit -m 'update' && git push origin master
